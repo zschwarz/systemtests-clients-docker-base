379b76a93f4384f01bb6c6cb6e847df7bc8abf6d
Zuzana Schwarzova <zschwarz@redhat.com>
2020-12-08 11:07:12 +0100

    Update script for downloading java clients to newer versions

--- download_java_clients.sh
+++ download_java_clients.sh
@@ -4,10 +4,14 @@ function install_java_client(){
     rm -rf ./build
     rm -rf ./cli-java
     mkdir -p build/java_clients
-    wget https://github.com/rh-messaging/cli-java/releases/download/v2.4.0er2/cli-activemq-1.2.2-SNAPSHOT-5.11.0.redhat-630377.jar -O build/java_clients/cli-activemq.jar
-    wget https://github.com/rh-messaging/cli-java/releases/download/v2.4.0er2/cli-artemis-jms-1.2.2-SNAPSHOT-2.7.0.redhat-00056.jar -O build/java_clients/cli-artemis-jms.jar
-    wget https://github.com/rh-messaging/cli-java/releases/download/v2.4.0er2/cli-qpid-jms-1.2.2-SNAPSHOT-0.42.0.redhat-00001.jar -O build/java_clients/cli-qpid-jms.jar
-    wget https://github.com/rh-messaging/cli-java/releases/download/v2.4.0er2/cli-paho-java-1.2.2-SNAPSHOT-RELEASE.jar -O build/java_clients/cli-paho-java.jar
+    git clone https://github.com/rh-messaging/cli-java
+    cd cli-java
+    mvn clean package
+    cd ..
+    cp cli-java/cli-activemq/target/cli-activemq-*-SNAPSHOT-*.jar build/java_clients/cli-activemq.jar
+    cp cli-java/cli-artemis-jms/target/cli-artemis-jms-*-SNAPSHOT-*.jar build/java_clients/cli-artemis-jms.jar
+    cp cli-java/cli-qpid-jms/target/cli-qpid-jms-*-SNAPSHOT-*.jar build/java_clients/cli-qpid-jms.jar
+    cp cli-java/cli-paho-java/target/cli-paho-java-*-SNAPSHOT-*.jar build/java_clients/cli-paho-java.jar
 }
 
 install_java_client
